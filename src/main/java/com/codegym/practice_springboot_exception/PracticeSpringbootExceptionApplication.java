58746fa0122c4441a13631af1d043d883cf9c299
GohanVu <hoangvhdata@gmail.com>
2024-07-22 09:08:15 +0700

    update

---
+++ src/main/java/com/codegym/practice_springboot_exception/PracticeSpringbootExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_springboot_exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringbootExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringbootExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/practice_springboot_exception/PracticeSpringbootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_springboot_exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
